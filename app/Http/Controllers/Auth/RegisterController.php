bfe49894f70981a9498d6a48094297d351c7f249
Manoar <tarikmanoar@gmail.com>
2020-02-02 21:51:17 +0600

    ==[T]== Add Mail varifications for Auth

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,12 +67,13 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return $user = User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'remember_token' => Str::random(32),
             'password' => Hash::make($data['password']),
         ]);
-        Mail::to($user->email)->send(new VarificationUser($user));
+        Mail::to($user['email'])->send(new VarificationUser($user));
+        return $user;
     }
 }
--- config/mail.php
+++ config/mail.php
@@ -11,8 +11,8 @@ return [
     | sending of e-mail. You may specify which one you're using throughout
     | your application here. By default, Laravel is setup for SMTP mail.
     |
-    | Supported: "smtp", "sendmail", "mailgun", "ses",
-    |            "postmark", "log", "array"
+    | Supported: "smtp", "sendmail", "mailgun", "mandrill", "ses",
+    |            "sparkpost", "postmark", "log", "array"
     |
     */
 
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.gmail.com'),
+    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'laravel6x@gmail.com'),
-        'name' => env('MAIL_FROM_NAME', 'Laravel User'),
+        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
     /*
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('laravel6x@gmail.com'),
+    'username' => env('MAIL_USERNAME'),
 
-    'password' => env('6xlaravel'),
+    'password' => env('MAIL_PASSWORD'),
 
     /*
     |--------------------------------------------------------------------------
